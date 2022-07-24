757804048ea2804f2191708920dd2264980349fd
Amelia Wibisono <ameliawibi@gmail.com>
2022-07-24 14:33:32 +0800

    useContext and useReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,106 @@
-import React, { useState, useEffect } from "react";
-import Uploadimage from "./Uploadimage";
-import GetFiles from "./GetFiles";
-import "./App.css";
+import React, { useReducer, useContext, useEffect, createContext } from "react";
+import axios from "axios";
 
-function App() {
-  const [data, setData] = useState(null);
+// Step 1: Initial State and Actions
+const initialState = {
+  imageList: [],
+};
+
+const actions = {
+  INITIALIZE: "INITIALIZE",
+  RESET: "RESET",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case actions.INITIALIZE:
+      return {
+        ...state,
+        imageList: action.payload,
+      };
+    case actions.RESET:
+      return {
+        imageList: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// Step 3: Create the Context and Provider to Dispatch the Actions.
+const ImageListContext = createContext();
+
+const Provider = ({ children }) => {
+  //Here we pass the reducer function and theinitialState to the useReducer hook. This will return state and dispatch. The state will have the initialState. And the dispatch is used to trigger our actions, just like in redux.
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    axios.get("/getfiles").then((response) => {
+      dispatch({
+        type: actions.INITIALIZE,
+        payload: response.data.files,
+      });
+    });
   }, []);
 
+  if (!state) return null;
+
+  const store = {
+    imageList: state.imageList,
+    resetImageList: (fileName) => {
+      dispatch({
+        type: actions.RESET,
+        payload: [fileName, fileName, fileName],
+      });
+    },
+  };
+
+  //We pass the value object as a prop to the TodoListContext's Provider, so that we can access it using useContext.
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-        <Uploadimage />
-        <GetFiles />
-      </header>
+    <ImageListContext.Provider value={store}>
+      {children}
+    </ImageListContext.Provider>
+  );
+};
+
+//Step 4: Create components that will use the store.
+function ViewImageList() {
+  const { imageList } = useContext(ImageListContext);
+
+  return (
+    <div>
+      <ul>
+        {imageList.map((image, index) => (
+          <li key={index}>{image.Key}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function ViewResetList() {
+  const { imageList, resetImageList } = useContext(ImageListContext);
+  const fileName = "test";
+  return (
+    <div>
+      <button
+        onClick={() => {
+          resetImageList(fileName);
+          console.log(imageList);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+//Step 5: Final step, wrapping the above two components to the Provider.
+export default function App() {
+  return (
+    <Provider>
+      <ViewImageList />
+      <ViewResetList />
+    </Provider>
+  );
+}
