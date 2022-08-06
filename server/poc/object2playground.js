14b8a8fc54902c2d056215a85140dbd8147afb1a
Amelia Wibisono <ameliawibi@gmail.com>
2022-08-06 11:24:06 +0800

    test playground

---
+++ server/poc/object2playground.js
@@ -0,0 +1,278 @@
+const result = {
+  traits: [
+    {
+      id: 97,
+      trait_type: "top",
+      probability: "1.00",
+      subtrait: "like-a-sir.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 98,
+      trait_type: "top",
+      probability: "1.00",
+      subtrait: "cowboy.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 99,
+      trait_type: "top",
+      probability: "1.00",
+      subtrait: "einstein.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 100,
+      trait_type: "top",
+      probability: "1.00",
+      subtrait: "fez.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 101,
+      trait_type: "top",
+      probability: "1.00",
+      subtrait: "cap.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 102,
+      trait_type: "background",
+      probability: "1.00",
+      subtrait: "wheat.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 103,
+      trait_type: "background",
+      probability: "1.00",
+      subtrait: "blue.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 104,
+      trait_type: "background",
+      probability: "1.00",
+      subtrait: "orange.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 105,
+      trait_type: "background",
+      probability: "1.00",
+      subtrait: "green.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 106,
+      trait_type: "head",
+      probability: "1.00",
+      subtrait: "light.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 107,
+      trait_type: "head",
+      probability: "1.00",
+      subtrait: "albino.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 108,
+      trait_type: "head",
+      probability: "1.00",
+      subtrait: "zombie.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 109,
+      trait_type: "head",
+      probability: "1.00",
+      subtrait: "mid.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 110,
+      trait_type: "head",
+      probability: "1.00",
+      subtrait: "skeleton.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 111,
+      trait_type: "facial-hair",
+      probability: "1.00",
+      subtrait: "mutton-chops.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 112,
+      trait_type: "facial-hair",
+      probability: "1.00",
+      subtrait: "goatee.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 113,
+      trait_type: "facial-hair",
+      probability: "1.00",
+      subtrait: "long-beard.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 114,
+      trait_type: "eyes",
+      probability: "1.00",
+      subtrait: "laser.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 115,
+      trait_type: "eyes",
+      probability: "1.00",
+      subtrait: "3d.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 116,
+      trait_type: "eyes",
+      probability: "1.00",
+      subtrait: "square-shades.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 117,
+      trait_type: "shirt",
+      probability: "1.00",
+      subtrait: "stripes-blue-white.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 118,
+      trait_type: "shirt",
+      probability: "1.00",
+      subtrait: "pinata.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 119,
+      trait_type: "shirt",
+      probability: "1.00",
+      subtrait: "purple-blue-pink.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+    {
+      id: 120,
+      trait_type: "shirt",
+      probability: "1.00",
+      subtrait: "flames.png",
+      rarity: "1.00",
+      collectionId: 12,
+      createdAt: "2022-08-05T15:50:29.536Z",
+      updatedAt: "2022-08-05T15:50:29.536Z",
+    },
+  ],
+};
+
+const layers = result.traits;
+//console.log(result.traits)
+const layersArray = [];
+
+let appended = new Set();
+
+for (const element of layers) {
+  const objToPush = {
+    name: element.trait_type,
+    probability: element.probability,
+    options: [],
+  };
+  //console.log(appended.has(JSON.stringify(objToPush)));
+  if (appended.has(JSON.stringify(objToPush))) {
+    continue;
+  } else {
+    layersArray.push(objToPush);
+    appended.add(JSON.stringify(objToPush));
+  }
+}
+
+for (const element of layers) {
+  const optionToPush = {
+    subtrait: element.subtrait,
+    rarity: element.rarity,
+  };
+  for (let i = 0; i < layersArray.length; i++) {
+    if (element.trait_type === layersArray[i].name) {
+      layersArray[i].options.push(optionToPush);
+    }
+  }
+}
+console.log(layersArray);
